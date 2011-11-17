85d36307c15c681e2a5112838cf313cb0cb6d297
Rushabh Mehta <rmehta@gmail.com>
2011-11-17 18:08:37 +0530

    framework cleanups and made save

--- js/app.js
+++ js/app.js
@@ -1,34 +1,9 @@
-$(document).ready(function() {
-	// empty div at the end
-	// if there are no empty items
-	var add_empty_div = function() {
-		if(!$('.wbitems .wbitem:empty').length) {
-			$('.wbitems').append('<div class="wbitem"></div>');
-		}
-	}
+// register plugins
+$._plugins.whiteboard = {path:'plugins/'}
+$.index = 'whiteboard';
 
-	// edit item on click
-	// if already in edit state, do nothing
-	$('.wbitems').delegate('div.wbitem','click', function() {
-		if($(this).find('input').length) return;
-		
-		var txt = $.trim($(this).text());
-		$(this).html('<input class="edit_wbitem" type="text" value="'+txt+'">')
-			.find('.edit_wbitem').focus();
-	});
-	
-	// set text on blur
-	// if text is empty, remove the item
-	$('.wbitems').delegate('input.edit_wbitem', 'blur', function() {
-		if(!$(this).val()) {
-			$(this).parent().remove();
-			add_empty_div();
-			return;
-		}
-		$(this).parent().html($(this).val());
-		add_empty_div();
-	});
-	
+$(document).ready(function() {
+	$('.brand').attr('href','#whiteboard');	
 });
 
 // make the sidebar
@@ -40,6 +15,33 @@ $(document).bind('session_load', function() {
 		// guest must either login or register
 		$('.sidebar').html('<a href="#signin">Login</a> or \
 			<a href="#register">Register</a> to save this whiteboard');
+	} else {
+		$('.sidebar').html('<a href="#" id="save_whiteboard">Save</a> this whiteboard');
+		$('#save_whiteboard').click(function() {
+			var label = $('.wblabel').text();
+			if(!label) {
+				alert('Must give a name!');
+				return;
+			}
+			if(!$('.wbitems .wbitem').length) {
+				alert("Can't save empty whiteboard");
+				return;
+			}
+			
+			var obj = {
+				"type":"whiteboard",
+				"owner":$.session.user,
+				"name": label.replace(/[^\w\d]+/g, '_').toLowerCase(),
+				"label": label,
+				"item":[]
+			}
+			$('.wbitems .wbitem').each(function(i, div) {
+				obj.item.push({"content": $(div).text()})
+			});
+			
+			$.objstore.post(obj, function() {
+				alert('saved!')
+			})
+		})
 	}
-})
-
+});
--- models/item.py
+++ models/item.py
@@ -10,10 +10,10 @@ class Item(model.Model):
 	create table `item` (
 		parent varchar(240) not null,
 		parent_type varchar(240) not null default "whiteboard",
-		idx index(10) not null,
+		idx int(10) not null,
 		content text,
 		color varchar(20) not null default "black",
-		size varchar(20) not null default "medium",
+		`size` varchar(20) not null default "medium",
 		_updated timestamp,
 		foreign key (parent) references whiteboard(name) on delete cascade,
 		unique (parent, idx)
